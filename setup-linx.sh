d44c7e81a7946fe54c43afbec2a230919d6e24b5
Ivan Bila <ivanbilag@gmail.com>
2020-06-22 13:38:11 +0200

    Update setup-linx.sh

--- setup-linx.sh
+++ setup-linx.sh
@@ -72,7 +72,7 @@ gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update
-sudo apt install yarn
+sudo apt install yarn -y
 yarn --version
 
 
