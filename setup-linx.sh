c1f6f561ddd6480cf2ad0e2282b96ac65035a497
Ivan Bila <ivanbilag@gmail.com>
2020-06-22 13:34:38 +0200

    Update setup-linx.sh

--- setup-linx.sh
+++ setup-linx.sh
@@ -173,3 +173,6 @@ sudo ln -sf /opt/telegram/Telegram /usr/bin/telegram
 curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
 echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
 sudo apt update && sudo apt install signal-desktop
+
+# ZSH
+chsh -s $(which zsh)
