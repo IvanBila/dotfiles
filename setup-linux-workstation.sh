ab5f3c171af19a67190cead4a80338f03694f994
Ivan Bila <ivanbilag@gmail.com>
2020-06-24 23:50:59 +0200

    Update setup-linux-workstation.sh

--- setup-linux-workstation.sh
+++ setup-linux-workstation.sh
@@ -176,3 +176,9 @@ sudo apt update && sudo apt install signal-desktop
 
 # ZSH
 chsh -s $(which zsh)
+
+# Uninstall Mysql 
+sudo apt-get purge mysql-server mysql-client mysql-common mysql-server-core-* mysql-client-core-*
+sudo rm -rf /etc/mysql /var/lib/mysql
+sudo apt-get autoremove
+sudo apt-get autoclean
