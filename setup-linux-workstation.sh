f4eaa17ce75c8344734d04f42525a89492c40fa2
Ivan Bila <ivanbilag@gmail.com>
2020-06-30 19:39:02 +0200

    Update setup-linux-workstation.sh

--- setup-linux-workstation.sh
+++ setup-linux-workstation.sh
@@ -85,7 +85,7 @@ ruby --version
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:ondrej/php
 sudo apt install php7.4 php7.4-common php7.4-opcache php7.4-cli php7.4-gd php7.4-curl php7.4-mysql -y
-sudo apt-get install -y php7.4 libapache2-mod-php7.4 php7.4-cli php7.4-common php7.4-mbstring php7.4-gd php7.4-intl php7.4-xml php7.4-mysql php7.4-mcrypt php7.4-zip php7.4-zip openssl php-common php-curl php-json php-mbstring php-mysql php-xml php-zip
+sudo apt-get install -y php7.4 libapache2-mod-php7.4 php7.3-bcmath php7.3-bz2 php7.3-soap php7.4-cli php7.4-common php7.4-mbstring php7.4-gd php7.4-intl php7.4-xml php7.4-mysql php7.4-mcrypt php7.4-zip php7.4-zip openssl php-common php-curl php-json php-mbstring php-mysql php-xml php-zip
 sudo apt install openssl php-common php-curl php-json php-mbstring php-mysql php-xml php-zip
 
 # PHP REPL
